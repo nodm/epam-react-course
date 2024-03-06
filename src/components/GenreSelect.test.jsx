cdea7ef721c491a7db629645802dbad66eafe1ad
nodm <nodm.pe@gmail.com>
2024-03-06 10:33:55 +0200

    T02: Add tests for the <GenreSelect>

---
+++ src/components/GenreSelect.test.jsx
@@ -0,0 +1,51 @@
+import { describe, expect, test, afterEach, beforeAll, vi } from 'vitest';
+import { render, screen, within } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import GenreSelect from './GenreSelect';
+
+const genreList = ['one', 'two', 'three', 'four', 'five'];
+const handleSelect = vi.fn();
+
+describe('Testing <GenreSelect />', () => {
+  beforeAll(() => {
+    userEvent.setup();
+  });
+
+  afterEach(() => {
+    handleSelect.mockClear();
+  });
+
+  test('should renders all genres passed in props', () => {
+    render(<GenreSelect genreList={genreList} initialGenre={genreList[3]} onSelect={handleSelect} />);
+
+    expect(screen.getAllByRole('button').length).toEqual(genreList.length);
+  });
+
+  test('should renders all genres passed in props (with snapshot)', () => {
+    const { container } = render(<GenreSelect genreList={genreList} initialGenre={genreList[3]} onSelect={handleSelect} />);
+
+    expect(container).toMatchSnapshot();
+  });
+
+  test('should highlights a selected genre passed in props', () => {
+    const activeGenre = genreList[3];
+
+    render(<GenreSelect genreList={genreList} initialGenre={activeGenre} onSelect={handleSelect} />);
+
+    const activeGenreItem = screen
+      .getAllByRole('listitem')
+      .find(element => within(element).queryByText(activeGenre));
+
+    expect(activeGenreItem).toHaveClass(/^border-red/);
+  });
+
+  test('should call "onChange" callback and passe correct genre in arguments after a click event on a genre button', async () => {
+    const selectedGenre = genreList[1];
+
+    render(<GenreSelect genreList={genreList} initialGenre={genreList[3]} onSelect={handleSelect} />);
+
+    await userEvent.click(screen.getByRole('button', { name: selectedGenre }));
+
+    expect(handleSelect).toHaveBeenCalledWith(selectedGenre);
+  });
+});
