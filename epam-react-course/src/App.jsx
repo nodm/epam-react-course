52093b314ec3012cd4abc6635d3ef67b4dfc9dc8
nodm <nodm.pe@gmail.com>
2024-03-22 17:05:02 +0200

    T03: Code refactoring

--- epam-react-course/src/App.jsx
+++ epam-react-course/src/App.jsx
@@ -1,11 +1,4 @@
 function App() {
-  const handleSearch = (query) => {
-    console.log(`Let's search for "${query}"`);
-  };
-  const handleGenreSelect = (genre) => {
-    console.log(`Selected genre "${genre}"`);
-  };
-
   return (
     <>
       <h1 className="text-3xl font-bold underline">
